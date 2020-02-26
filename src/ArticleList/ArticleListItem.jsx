877b52c0c5f0c2e91e956e233b96bc2cd975a48e
Yuanqing Li <yuanqing@Yuanqings-MacBook-Pro.local>
2020-02-26 10:21:36 -0800

    Assignment 7

--- src/ArticleList/ArticleListItem.jsx
+++ src/ArticleList/ArticleListItem.jsx
@@ -6,19 +6,13 @@ import styles from "./ArticleListItem.module.css";
 const ArticleListItem = ({article}) => {
 	return (
 		<section className={styles.section}>
-			<div className={styles.left}>
-				<ArticleImage url={article.image._url} title={article.title}/>
-			</div>
-			<div className={styles.right}>
-				<h2 className={styles.header}>{article.title}</h2>
-				<p className={styles.start}>{article.shortText}</p>
-				<div className={styles.info}> 
-					<time className={styles.bold}>{article.pubDate}</time>			
-				</div>
-				<div className={styles.button}>
-					<SlugButton buttonText = {article.author} slug = {article.slug}/>
-				</div>
-			</div>
+			<ArticleImage className={styles.image} url={article.image._url} title={article.title}/>
+			<h2 className={styles.header}>{article.title}</h2>
+			<p className={styles.start}>
+				{article.shortText}
+				<time className={styles.info}>{article.pubDate}</time>
+			</p>			
+			<SlugButton buttonText = {article.author} slug = {article.slug}/>
 		</section>
 	);
 }
